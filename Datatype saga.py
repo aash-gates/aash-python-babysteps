5801bd0700c789163860feef319bdbbfdbe63be5
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-02 06:56:18 +0530

    Create Datatype saga.py

---
+++ Datatype saga.py
@@ -0,0 +1,9 @@
+#Prints the String Hellp World
+print("hello world")
+#prints the Boolean value
+print(True)
+#prints floting point value
+print(1.5)
+#prints integer value
+print(23)
+print('This is Aash Gates.Official')
