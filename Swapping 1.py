e87ceef97d5814739af8f5e4731bb670cfdc16d3
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-03 23:44:52 +0530

    Create Swapping 1.py

---
+++ Swapping 1.py
@@ -0,0 +1,10 @@
+x = 10
+y = "ten"
+x = y 
+# x now has the value of y which is String "ten".
+
+y = x
+# y now has the value of x which is basically string value "ten" again because we earlier assigned y value to x .
+
+print (x)
+print (y)
