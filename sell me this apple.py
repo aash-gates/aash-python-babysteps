dc7b7f2c6506763c2a49ddf3daf558507049b6c0
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-04 11:14:46 +0530

    Create sell me this apple.py

---
+++ sell me this apple.py
@@ -0,0 +1,34 @@
+'''
+
+Let’s say you have an apple and your friend has money. 
+Your friend wants to buy your apple and you agree to sell 
+him the apple so you will exchange the apple for his money.
+
+Note: write program with the help of temp variable 
+
+'''
+
+you = "Apple"
+friend = "money"
+
+#scene 1
+
+print ("You have an",you)
+print ("Your Friend has",friend)
+
+#thinking
+print ("Your friend wants to buy",you,"from you by giving you",friend)
+
+print ("Selling process going on wait.................")
+
+temp = you # you checking the weight of the apple you are going to sell on a scale.
+you = friend # friend has paid you for the apple .
+friend = temp #you sell the apple which was sitting on the scale.
+
+#scene 2
+
+print ("Your friend now has",friend)
+print ("and")
+print ("You have the" ,you)
+
+#end of the program 
