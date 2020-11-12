9d4fd666e1116ce2f57996187c0bc50f81f0a921
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-12 12:10:49 +0530

    Create arithematic 2.py

---
+++ arithematic 2.py
@@ -0,0 +1,9 @@
+Var1 = 12.5 + 2.5   # Addition 
+Var2 = 12.5 - 2.5   # Subtraction 
+Var3 = 12.5 * 2.5   # Multiplication 
+Var4 = 12.5 / 2.5   # Division
+
+print(Var1)         # float
+print(Var2)         # float
+print(Var3)         # float
+print(Var4)         # float
