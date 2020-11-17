ea3f6676a5d9a834f4827f390db36fb4ac9fd673
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-17 06:37:30 +0530

    Create Datatype saga 2.py

---
+++ Datatype saga 2.py
@@ -0,0 +1,17 @@
+#Prints the String Hellp World
+print("hello world")
+import time
+time.sleep(0.5)
+#prints the Boolean value
+print(True)
+import time
+time.sleep(0.5)
+#prints floting point value
+print(1.5)
+import time
+time.sleep(0.5)
+#prints integer value
+print(23)
+print('This is Aash Gates.Official')
+
+#end of the Program
