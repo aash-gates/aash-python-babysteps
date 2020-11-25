881f3c3e490ae647a640d05fba40dba990540744
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-11-25 12:45:03 +0530

    /

--- Ch1mod5testcode1.py
+++ Ch1mod5testcode1.py
@@ -1,7 +1,32 @@
 '''
 practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
 by Aashik J Krishnan/Aash Gates
+
 '''
 x = 5
 
+print(x)
+
+x=5
+x += 5
+print(x)
+
+x = 5
+x *=3
+print(x)
+
+x = 5
+x = x - 20
+print (x)
+
+x = 5
+x**=2
+print(x)
+
+x = 5
+x *="Num"
+print(x)
+
+x = 5
+x +="py"* 2
 print(x)
