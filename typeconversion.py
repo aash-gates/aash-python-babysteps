d30bb21614378a2daf5380928105558a2593895b
Aashik J Krishnan <58915216+Aash-Gates@users.noreply.github.com>
2020-12-04 07:16:36 +0530

    Create typeconversion.py

---
+++ typeconversion.py
@@ -0,0 +1,14 @@
+#testing out Type Conversion
+x = (input("Please enter your name:  "))
+
+import time
+time.sleep(0.89)
+
+print('saving your Good name in our Database..........')#just for fun :D
+
+import time
+time.sleep(2.0) #i love this thing the delay makes it look so realastic 😂😂
+
+print("Hi " + x, "have a great time learnig python")
+
+#end-of-the-program
