dc03276a9091f6b6a04b8ccbeedab11103a16e2c
aash-gates <aashgates@outlook.com>
2021-05-27 22:08:45 +0530

    Create typeconversion 3.py

---
+++ typeconversion 3.py
@@ -0,0 +1,16 @@
+#typeconversion to convert a float and int to str
+
+x = 1.5
+print (x)
+print(type(x))
+
+y = 3
+print(y)
+print(type(y))
+
+print(str(x))
+print(type(str(x)))
+print(str(y))
+print(type(str(y)))
+
+#emd-of-the-program
