be22106686fdc30e87acf852b0695d9a0b3e329c
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-04 19:46:28 +0530

    Update if else statement 2.py

--- if else statement 2.py
+++ if else statement 2.py
@@ -13,3 +13,19 @@ else  :
     print("The Number is not Positive")
 
 #end of the program
+
+#if else statemnet version 2 (IBm Digital Nation)
+
+num = 15
+
+if num > 0 :
+    if num < 20 :
+        
+      print("The Number is Positive and less than 20")
+    else : 
+        print("The Number is Positive") #since its 15 which is less than 15 this Condition will be printed first
+        
+else  : 
+    print("The Number is not Positive")
+
+#end of the program
