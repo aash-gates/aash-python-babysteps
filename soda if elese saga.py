ec13a4d9f35cb32b92b9eadd83147ee275780af7
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-05 19:14:21 +0530

    Create soda if elese saga.py

---
+++ soda if elese saga.py
@@ -0,0 +1,13 @@
+#A program to do equality comparasion (IBM Digital Nation Africa)
+
+drink = "Water"
+
+if drink == "soda" :
+    print("I will Purchase Soda")
+    
+else :
+    if drink == "Juice" :
+       print("I will Purchase Juice")
+       
+    else : 
+        print(" I will Purchase Water ")
