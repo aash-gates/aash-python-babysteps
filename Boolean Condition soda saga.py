368c845f4bdf70ffa809c6a3a3f7d941322fa6ee
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-05 20:43:53 +0530

    Update Boolean Condition soda saga.py

---
+++ Boolean Condition soda saga.py
@@ -0,0 +1,9 @@
+is_water = input("Is water available (True or False)?")
+
+if is_water == "True":
+    is_water = True
+else:
+    is_water = False
+
+if is_water == True:
+    print("I will purchase this drink")
