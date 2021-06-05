ade55e89c42fbe3eed03b856a225b7d78a41f038
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-05 19:31:40 +0530

    Update Soda Saga shortned Version.py

---
+++ Soda Saga shortned Version.py
@@ -0,0 +1,13 @@
+#A program to do equality comparasion in shorter form (IBM Digital Nation Africa)
+
+drink =  input("What Drink is available : ")
+
+if drink == "soda" :
+    print ("I will Purchase Soda")
+    
+elif drink == "Juice" :
+    print ("I will Purchase Juice")
+
+else : 
+    print ("I will Purchase Water")
+    
