c9103c39b301ad88cdf938786b641c297d47267b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-10 22:38:35 +0530

    Update or operator 2.py

--- or operator 2.py
+++ or operator 2.py
@@ -8,8 +8,11 @@ if (fever or cough or Fatigue) :
     print ("I will see a Docotor")
     
 import time
+
 time.sleep(3.00)
     
 import webbrowser
 
 webbrowser.open('https://youtu.be/7t9e81KYeBM?t=21')  #Let's see the Docotor
+
+#end-of-the-program 
