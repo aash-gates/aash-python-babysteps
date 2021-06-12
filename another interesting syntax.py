59aef695b49a905365e0ee692fdcd15d2fa7ca5e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-13 00:00:46 +0530

    Update another interesting syntax.py

--- another interesting syntax.py
+++ another interesting syntax.py
@@ -3,5 +3,5 @@ money =   10.00  # Enter an amount here before running the program
 
 if money > cost:
     print("I can afford the Apple")
-    else
+else :
     print("I Can't afford this Damn Apple")
