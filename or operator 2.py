7929ca2e703b7675d9d03d884b6d51dcacb83f25
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-12 22:32:38 +0530

    Update or operator 2.py

--- or operator 2.py
+++ or operator 2.py
@@ -4,15 +4,16 @@ fever = True
 cough = False
 Fatigue = False
 
+print ('Checking You ')
+
 if (fever or cough or Fatigue) :
     print ("I will see a Docotor")
     
-import time
 
-time.sleep(3.00)
-    
 import webbrowser
 
-webbrowser.open('https://youtu.be/7t9e81KYeBM?t=21')  #Let's see the Docotor
+webbrowser.open('https://youtu.be/7t9e81KYeBM?t=21')  #Let's Visit the Docotor
 
 #end-of-the-program 
+
+#tryoutprogram
