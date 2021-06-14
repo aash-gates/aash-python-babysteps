94c8eeee43a8eaf4db98a4d4fd27555689caedbe
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-14 22:39:14 +0530

    Update not Bool.py

--- not Bool.py
+++ not Bool.py
@@ -1,4 +1,8 @@
 #not Operator Boolean IBM Digital Nation
 the_bool = False
 
-if the_bool
+if the_bool == False:
+    print("the_bool is False")
+if the_bool != True :
+    print("the_bool is false(not true)")
+    
