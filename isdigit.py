02bb946c7a593e2fd28b7cc05b12dee8d1f1245d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-17 23:24:01 +0530

    Update isdigit.py

---
+++ isdigit.py
@@ -0,0 +1,7 @@
+#checking the string for digit IBM Digital Nation
+
+only_digits ="1234567890"
+print(only_digits.isdigit()) #returns True
+
+has_digit = "AfG89K"
+print(has_digit.isdigit()) #returns False
