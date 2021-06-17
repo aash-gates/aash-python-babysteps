e5856dd49156a1e1a85f666f101329d962c6161f
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-17 16:14:57 +0530

    Create lowecarse string.py

---
+++ lowecarse string.py
@@ -0,0 +1,12 @@
+# program to correct the case of the string IBM Digital Nation
+
+string = ("Aashik Socially known as Aash Gates")
+print (string)
+
+import time
+time.sleep(1.00)
+
+mystring = string.lower
+
+print (mystring)
+
