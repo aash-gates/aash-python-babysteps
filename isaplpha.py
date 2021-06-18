26bf149bc9d63511637e1a7b0d60404248752700
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-18 19:34:45 +0530

    Create isaplpha.py

---
+++ isaplpha.py
@@ -0,0 +1,10 @@
+#checking if the string value is only composed of alpabets
+
+only_letters ="abcdefg"
+print(only_letters.isalpha())
+
+has_letters = "shyam9553"
+print(has_letters.isalpha())
+
+no_letters ="1234567890"
+print(no_letters.isalpha())
