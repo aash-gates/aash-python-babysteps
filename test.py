53833a364809439f05897d4fae8bc690aee6c025
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-18 20:14:29 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+string = "aashikJKrishnan12853@"     # Change the string text between the quotation marks to see what the program does
+
+if string.isspace():
+    print("only white spaces")
+	
+elif string.isalpha():
+    print("only letters")
+	
+elif string.isdigit():
+    print("only digits")
+	
+elif string.isalnum():
+    print("digits and letters")
+	
+else:
+    print("digits, letters, and/or symbols")
