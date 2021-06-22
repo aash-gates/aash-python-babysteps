455a70f92d07bd57c7bb2f726ccd8e5e82b4f699
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-22 12:52:03 +0530

    Update index fix.py

--- index fix.py
+++ index fix.py
@@ -1,8 +1,3 @@
-#a Test program to over come the Index Error IBM Digital Nation
-
 string = "Alphabet"
-last_char = string[0]
-
-print(last_char.isalpha())
-
-#end of test program
+last_char = string[-1]
+print(last_char)
