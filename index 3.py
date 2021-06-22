0160359e6b9cda75c645173524c1e9959b42ac3e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-22 12:23:17 +0530

    Update index 3.py

--- index 3.py
+++ index 3.py
@@ -1,6 +1,6 @@
 # a program to find the string using the n- function IBM Digital Nation
 
 string ="Aash Gates"
-last_char = string[len(string) - 2]
+last_char = string[len(string) - 3]
 
 print(last_char)
