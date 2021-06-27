10fee74582f59fa076fc3c439a95ef33d2c4b4ba
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-27 20:14:17 +0530

    Create List Syntax pro.py

---
+++ List Syntax pro.py
@@ -0,0 +1,14 @@
+# A syntax program to Demonstrate the use of the List function
+'''Syntax progeam'''
+
+lst_1 =  list()
+print(lst_1)
+
+lst_2 = []
+print(lst_2)
+
+'''pronka pronk nothing worked '''
+
+#this was a demo program
+
+#end of the Program
