72a3f1beb9baf8a3dc09a5b55f541cdadd6119c0
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-28 22:23:40 +0530

    Create Datatype saga 2.py

---
+++ Datatype saga 2.py
@@ -0,0 +1,22 @@
+'''
+practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
+by Aashik J Krishnan/Aash Gates
+'''
+
+#Prints the String Hellp World
+print("hello world")
+import time
+time.sleep(0.5)
+#prints the Boolean value
+print(True)
+import time
+time.sleep(0.5)
+#prints floting point value
+print(1.5)
+import time
+time.sleep(0.5)
+#prints integer value
+print(23)
+print('This is Aash Gates.Official')
+
+#end of the Program
