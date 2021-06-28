c83db85d425282b559fe5f9716a2e525e0bdac96
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-28 22:27:52 +0530

    Create soda saga and operatot.py

---
+++ soda saga and operatot.py
@@ -0,0 +1,17 @@
+#AND Operator IBM Digital Nation Africa
+
+is_soda = True
+have_money = False
+is_thisty = False
+
+if is_soda and have_money and is_thisty :
+    print ("I will Purchase this drink")
+else :
+    print ("I will not Purchase this drink")
+if have_money == False :
+    print("I don't have money")
+if is_soda == False :
+    print("soda is not available")
+    
+if is_thisty == False :
+    print("I am not Thirsty")
