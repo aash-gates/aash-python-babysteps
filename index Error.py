97721c97a145086610667baf5b646cb3c587f1cb
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-06-29 18:39:22 +0530

    Delete index Error.py

--- index Error.py
+++
@@ -1,7 +0,0 @@
-# A demo program to show the Indexing Error IBM Digital Nation
-
-grocery_list=["Milk","Eggs","Bread","Rice", 20.00]
-
-print (grocery_list[5]) #Prints an Error Because the Index value Does not Exist(list index out of range)
-
-#end of the Program
