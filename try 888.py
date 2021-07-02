b576cfb27b9c8aad01e01c22851c81ae1218bdc4
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-02 21:56:18 +0530

    Create try 888.py

---
+++ try 888.py
@@ -0,0 +1,9 @@
+num = int(input("Please enter an integer value"))
+
+if num > 0:
+    if num < 20:
+        print("The number is positive and less than 20")
+    else:
+      print("The number is positive")
+else:
+    print("The number is not positive")
