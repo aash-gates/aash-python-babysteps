b3d3090c89e410d1af8d2612ff787b3119862a5c
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-06 22:15:59 +0530

    Create negative indexing.py

---
+++ negative indexing.py
@@ -0,0 +1,10 @@
+#A demo program to demonstrate the negative indexing of the String IBM Digital Nation Courses
+
+string = "Alphabet"
+last_char = string[-1]
+
+last_char= last_char.capitalize()
+
+print(last_char)
+
+#end of the program
