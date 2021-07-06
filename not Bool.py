471ffd1c5b13950201789cc472a782215ec2ba28
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-06 22:00:52 +0530

    Delete not Bool.py

--- not Bool.py
+++
@@ -1,9 +0,0 @@
-#not Operator Boolean IBM Digital Nation
-the_bool = False
-
-if the_bool == False:
-    print("the_bool is False")
-if the_bool != True :
-    print("the_bool is false(not true)")
-    
-#end-of-the-program
