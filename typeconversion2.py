d3183550323aadb8f217b8cb86ba8567433aa6a1
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-07 23:43:54 +0530

    Create typeconversion2.py

---
+++ typeconversion2.py
@@ -0,0 +1,12 @@
+num1 = 1.655 #initilizing a float value 
+num2 = int(num1)  #typeconverting float to int
+print(num2) #results
+
+
+
+str = ("1.555") #we are assigning string value to the variable
+num3 = int(float(str)) #converting from string to float then from float to string
+print(num3) #results
+
+#end of the program
+
