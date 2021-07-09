7d741ed5f8730e4c8a1659c28492298d65e29312
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 23:46:51 +0530

    Create upper method.py

---
+++ upper method.py
@@ -0,0 +1,17 @@
+#coverting the string to upper case IBM Digital Nation
+
+string = "Aashik Socially known as aash Gates"
+print(string)
+
+import time
+time.sleep(0.33)
+
+print("converting the string to upper case")
+
+import time
+time.sleep(1.00)
+
+new_string = string.upper()
+print(new_string)
+
+#end of the Program
