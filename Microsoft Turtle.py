55acbf5bc75fe94a6a5411c37f042b7f3cd77453
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 10:55:02 +0530

    Update Microsoft Turtle.py

--- Microsoft Turtle.py
+++ Microsoft Turtle.py
@@ -10,7 +10,8 @@ for _ in range(4):
   t.left(90) # Turn turtle by 90 degree
 t.end_fill()
 
-
+t.color("black")
+t.begin_fill()
 t.forward(100)
 for _ in range(4):
   t.forward(s) # Forward turtle by s units
