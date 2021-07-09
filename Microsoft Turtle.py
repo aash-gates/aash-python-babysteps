47028bec516ecdd16198ed1fba0566717d6e0c47
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 10:54:22 +0530

    Update Microsoft Turtle.py

--- Microsoft Turtle.py
+++ Microsoft Turtle.py
@@ -3,12 +3,12 @@ import turtle
 t = turtle.Turtle()
  
 s = 100
-t.color("black")
-t.begin_fill()
+tr.color("black")
+tr.begin_fill()
 for _ in range(4):
   t.forward(s) # Forward turtle by s units
   t.left(90) # Turn turtle by 90 degree
-t.end_fill()
+
 
 t.forward(100)
 for _ in range(4):
