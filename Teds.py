5f2a582c5b937dd29ecb4c9ed917d5b1d9615183
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 11:39:05 +0530

    Update Teds.py

--- Teds.py
+++ Teds.py
@@ -1,6 +1,7 @@
-from turtle import *
-color('red', 'yellow')
-pencolor()
-'red'
-fillcolor()
-'yellow'  
+import turtle
+t= turtle.Turtle()
+turtle.Screen.colormode(255)
+t.color(243, 79, 28)
+t.begin_fill()
+t.circle(50)
+t.end_fill()
