819b8d37cc59961df70d1a3dcd5cb754ff5da02e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 10:43:03 +0530

    Update Microsoft Turtle.py

--- Microsoft Turtle.py
+++ Microsoft Turtle.py
@@ -12,6 +12,13 @@ for _ in range(4):
 t = turtle.Turtle() 
 s = 100
 t.forward(100)
+for _ in range(8):
+  t.forward(s) # Forward turtle by s units
+  t.left(90) # Turn turtle by 90 degree
+  
+t = turtle.Turtle() 
+s = 100
+t.left(100)
 for _ in range(4):
   t.forward(s) # Forward turtle by s units
   t.left(90) # Turn turtle by 90 degree
@@ -19,6 +26,13 @@ for _ in range(4):
 t = turtle.Turtle() 
 s = 100
 t.left(270)
+for _ in range(4):
+  t.forward(s) # Forward turtle by s units
+  t.left(90) # Turn turtle by 90 degree
+  
+t = turtle.Turtle() 
+s = 100
+t.left(100)
 for _ in range(4):
   t.forward(s) # Forward turtle by s units
   t.left(90) # Turn turtle by 90 degree
