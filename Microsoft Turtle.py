109c9b4d4b440d066fe78a82e6d7ffd6c48cfc80
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 13:06:19 +0530

    Update Microsoft Turtle.py

--- Microsoft Turtle.py
+++ Microsoft Turtle.py
@@ -6,7 +6,7 @@ turtle.setworldcoordinates(-300, -300, 300, 300)
 
 t = turtle.Turtle()
 t.speed(2)
- 
+turtle.Screen().bgcolor("black")
 s = 100
 t.screen.colormode(255)
 t.color(243, 79, 28)
@@ -43,3 +43,6 @@ for _ in range(4):
   t.left(90) # Turn turtle by 90 degree
 t.end_fill()
 t.delay(50)
+
+
+
