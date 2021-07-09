8d6def1e826f3b5d7ccec07b832eb4967ac12315
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-09 12:10:56 +0530

    Update Microsoft Turtle.py

--- Microsoft Turtle.py
+++ Microsoft Turtle.py
@@ -29,7 +29,8 @@ for _ in range(4):
   t.left(90) # Turn turtle by 90 degree
 t.end_fill()
 
-t.color("Yellow")
+t.screen.colormode(255)
+t.color(243, 79, 28)
 t.begin_fill()
 t.right(450)
 for _ in range(4):
