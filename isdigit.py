330dc076461c77c82e275948d5924670a87bfdfa
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-12 23:42:45 +0530

    Create isdigit.py

---
+++ isdigit.py
@@ -0,0 +1,18 @@
+#checking the string for digit IBM Digital Nation
+
+
+only_digits ="1234567890"
+print(only_digits.isdigit()) #returns True
+
+
+has_digit = "AfG89K"
+print(has_digit.isdigit()) #returns False
+
+
+no_digit = "Onlyfans"
+print(no_digit.isdigit()) #returns false
+
+#end of the Program
+
+
+
