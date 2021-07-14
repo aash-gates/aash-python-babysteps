2e353647d0bdaea9707ec93e7f6ef14b087a4b10
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-14 23:52:57 +0530

    Delete Multiple if statements.py

--- Multiple if statements.py
+++
@@ -1,9 +0,0 @@
-#multiple if statements (IBM Digital Nation Africa)
-
-num = 72.5
-
-if num > 0 :
-    print ("The Number is positive")
-    
-if num > 20 :
-    print("The Number is greater than 20")
