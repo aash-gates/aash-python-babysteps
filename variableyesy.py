e1b62c3e5ab37193da5b0b4449a25775eca38618
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-14 23:59:26 +0530

    Create variableyesy.py

---
+++ variableyesy.py
@@ -0,0 +1,12 @@
+'''
+practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
+by Aashik J Krishnan/Aash Gates
+'''
+
+#testing variables
+x = 25
+print (x)
+x = 30
+print (x)
+
+#end of the Program
