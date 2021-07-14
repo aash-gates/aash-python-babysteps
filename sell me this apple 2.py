e639ff3a4b099476c9046683fb66b3fbf6fc2e87
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-14 23:57:31 +0530

    Create sell me this apple 2.py

---
+++ sell me this apple 2.py
@@ -0,0 +1,16 @@
+'''
+practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
+by Aashik J Krishnan/Aash Gates
+'''
+
+you = "Apple"
+friend = "money"
+
+#swap function
+
+you,friend = friend,you
+
+print (you)
+print (friend)
+
+#end-of-the-program
