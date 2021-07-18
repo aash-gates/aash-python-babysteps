6ab52a8a9837d6647a8272aadd772199d38e8b0d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-18 23:04:17 +0530

    Create Variable arithmetic.py

---
+++ Variable arithmetic.py
@@ -0,0 +1,11 @@
+'''
+practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
+by Aashik J Krishnan/Aash Gates
+'''
+
+var1 = 10
+var2 = var1 + 2
+
+print (var1 ,var2)
+
+#end of the Program
