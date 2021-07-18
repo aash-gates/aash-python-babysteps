41f5b963ba1f3585b1ed0bf29287aa044d70618e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-18 22:47:34 +0530

    Create power operation.py

---
+++ power operation.py
@@ -0,0 +1,24 @@
+'''
+practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
+by Aashik J Krishnan/Aash Gates
+'''
+
+num1 = 5 ** 2 #assigning power to 5
+print(num1)
+
+import time
+time.sleep(1.5)
+
+num2 = 5 ** 2
+
+import time
+time.sleep(1.5)
+
+print(num2)
+
+num3 = 5 ** 2.0
+num4 = 5.0 ** 2
+print(num3)
+print(num4)
+
+#end of the program 
