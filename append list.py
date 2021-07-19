967af474f5cbcdf9e92940334d1545e6dfa52277
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-19 21:42:47 +0530

    Update append list.py

--- append list.py
+++ append list.py
@@ -1,2 +1,9 @@
 #append() method IBM Digital Nation Courses
-GroceryList.append ("milk")
+grocery_list=[]
+grocery_list.append ("milk")
+grocery_list.append ("eggs")
+grocery_list.append("bread")
+grocery_list.append("Rice")
+
+print("grocery_list")
+print(len(grocery_list))
