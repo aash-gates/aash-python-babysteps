29c26eaae68c667ac42ec89b038d853303e943b6
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:18:16 +0530

    Create negative index.py

---
+++ negative index.py
@@ -0,0 +1,10 @@
+#negative Indexing,List Module IBM Digital Nation
+
+grocery_list= ["Milk","Eggs","Bread","rice", 20.00]
+print (grocery_list [-1])
+print (grocery_list[-2])
+print (grocery_list[-3])
+print (grocery_list[-4])
+print (grocery_list[-5])
+
+#end of the program
