e37bef0084a439c3958e174a37b2d53351d5e69b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:16:01 +0530

    Create index Error.py

---
+++ index Error.py
@@ -0,0 +1,7 @@
+# A demo program to show the Indexing Error IBM Digital Nation
+
+grocery_list=["Milk","Eggs","Bread","Rice", 20.00]
+
+print (grocery_list[5]) #Prints an Error Because the Index value Does not Exist(list index out of range)
+
+#end of the Program
