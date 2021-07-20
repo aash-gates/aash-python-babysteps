29a08bf577c25be3605857711b36c286961ea3d2
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:00:42 +0530

    Create Capitalize string.py

---
+++ Capitalize string.py
@@ -0,0 +1,16 @@
+# program to capitalize the string IBM Digital Nation
+
+
+string = "aash Gates Socially Know as Aashik"
+print (string)
+
+print ("reprasing the string \n Please wait.....")
+
+import time
+time.sleep(1.00)
+
+new_string = string.capitalize() #calling capitalize method 
+
+print (new_string)
+
+#end-of-the-program
