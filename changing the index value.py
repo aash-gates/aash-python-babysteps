fb4e5b4633d7ee36849a7c2c78e1372123e31176
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:00:49 +0530

    Create changing the index value.py

---
+++ changing the index value.py
@@ -0,0 +1,8 @@
+#changing the Index value Module List IBM Digital Nation
+
+grocery_list= ["Milk","Eggs","Bread","rice",20.00]
+print(grocery_list)
+import time
+time.sleep(0.99)
+grocery_list[0] = "Juice"
+print(grocery_list)
