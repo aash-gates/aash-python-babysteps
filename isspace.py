e8df7684323f68cfb1b67b7ea2019bde7168bbf9
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:01:58 +0530

    Create isspace.py

---
+++ isspace.py
@@ -0,0 +1,12 @@
+#a program to checking the white space IBM Digital Nation
+
+only_space = " "
+print(only_space.isspace())
+
+has_space = "123  9"
+print(has_space.isspace())
+
+no_space = "aash"
+print(no_space.isspace())
+
+#end of the Program
