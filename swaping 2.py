418e21a56874365ecda2a692d37dc424ca913e5d
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:03:47 +0530

    Create swaping 2.py

---
+++ swaping 2.py
@@ -0,0 +1,18 @@
+'''
+practice qusestion from chapter 1 Module 5 of IBM Digital Nation Courses
+by Aashik J Krishnan/Aash Gates
+'''
+
+x = 10
+y = "ten"
+
+#step 1
+
+x,y = y,x 
+
+#printing on next line
+
+print(x)
+print(y)
+
+#end of the program
