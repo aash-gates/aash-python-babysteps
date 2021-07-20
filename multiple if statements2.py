9af972294f27f6b7d60b0db53a4189b7b71b7bb5
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-20 23:02:28 +0530

    Create multiple if statements2.py

---
+++ multiple if statements2.py
@@ -0,0 +1,25 @@
+#multiple if statements aash gates style
+
+num = int (input("input a Number  :  "))
+
+print("The Number you have entered is",num)
+
+import time
+time.sleep(0.55)
+
+
+if num > 0 :
+    print ("The Number is Positive")
+    
+import time
+time.sleep(0.55)
+    
+if num > 20 :
+    print (num, "is greater than 20")
+    
+import time
+time.sleep(1.00)
+
+print("Thanks for using this Program")
+
+#end-of-the-program
