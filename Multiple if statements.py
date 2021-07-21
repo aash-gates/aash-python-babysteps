7b43dda309bc4a6ac8a4962bdf649455a63fd7c2
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-21 13:03:44 +0530

    Create Multiple if statements.py

---
+++ Multiple if statements.py
@@ -0,0 +1,9 @@
+#multiple if statements (IBM Digital Nation Africa)
+
+num = 72.5
+
+if num > 0 :
+    print ("The Number is positive")
+    
+if num > 20 :
+    print("The Number is greater than 20")
