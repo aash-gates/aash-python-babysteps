ffc9cf7573ff5b994e03fe5539d5b79397097cc5
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-21 13:08:39 +0530

    Create append Funtion.py

---
+++ append Funtion.py
@@ -0,0 +1,12 @@
+#a demo program on append function and list IBM Digital Nation
+
+grocery_list = []
+grocery_list.append("Milk")
+grocery_list.append("Eggs")
+grocery_list.append("Bread")
+grocery_list.append("Rice")
+
+print (grocery_list)
+print (len(grocery_list))
+
+#end of the Program
