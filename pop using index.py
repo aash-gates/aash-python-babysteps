bbe3995fae550ffa84f03ba4518891997aedc91b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-21 16:59:42 +0530

    Update pop using index.py

--- pop using index.py
+++ pop using index.py
@@ -1,6 +1,6 @@
 #pop() method using the index of the array from List Module IBM Digital Nation
 
-my_list = ['a', 'b', 'c', 'd', 'e']
+my_list = [1,2,3,"Hello","World"]
 print(my_list.pop(2))
 
 print(my_list)
