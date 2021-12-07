b2686a9fd07fcd1d252960b312ed85970d6e1b69
Aashik J Krishnan <aashgates@Outlook.com>
2021-12-07 22:55:46 +0530

    Delete pop and Store method.py

--- pop and Store method.py
+++
@@ -1,9 +0,0 @@
-# A Demo Program on of Pop and Store method from List Module of IBM Digital National University
-
-my_list = [1 ,2 ,3 ,"Hello","World"]
-
-last_item = my_list.pop()
-
-print(last_item)
-
-#end of the program
