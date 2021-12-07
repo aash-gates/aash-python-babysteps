5c82553fb2287f31757e3421f0ef5209ea9b0ae6
Aashik J Krishnan <aashgates@Outlook.com>
2021-12-07 23:05:56 +0530

    Create Boolean Condition soda saga.py

---
+++ Boolean Condition soda saga.py
@@ -0,0 +1,14 @@
+#Booleran Condition on Soda program (IBM Digital Nation)
+
+
+is_water = input("Is water available (True or False)?")
+
+if is_water == "True":
+    is_water = True
+else:
+    is_water = False
+
+if is_water == True:
+    print("I will purchase this drink")
+    
+#end-of-the-program.
